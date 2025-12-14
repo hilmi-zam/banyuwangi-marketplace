6dd37551631a1657596b67cb272fd30a75f3ca39
Hilmi Zam <hilmi.zam@example.com>
2025-12-14 20:34:33 +0700

    feat(vendorC): Tambah struktur dasar data Vendor C Resto dengan item pertama Nasi Tempong

---
+++ vendors/vendorC/vendorC-resto.js
@@ -0,0 +1,21 @@
+/**
+ * Vendor C - Resto & Kuliner
+ * Mahasiswa 3 - Data Makanan & Minuman
+ */
+
+const restoData = [
+  {
+    id: 501,
+    details: {
+      name: "Nasi Tempong",
+      category: "Food"
+    },
+    pricing: {
+      base_price: 20000,
+      tax: 2000
+    },
+    stock: 50
+  }
+];
+
+module.exports = restoData;
