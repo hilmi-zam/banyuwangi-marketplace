9a8dcc17341266a8ff3c273f77e56ef7ea008db8
MuhAinulHuda24 <mainulhuda2005@gmail.com>
2025-12-14 22:13:06 +0700

    menambah produk teh celup dan kripik pisang

---
+++ vendors/vendorA/vendorA-Warung_Legacy.js
@@ -0,0 +1,29 @@
+/**
+ * Vendor A - Simulasi Data Produk dari Vendor A
+ * 
+ * File ini hanya berisi data produk Vendor A
+ * Tidak ada logic integrasi atau parsing data vendor lain
+ */
+
+const vendorAData = [
+  {
+    kd_produk: "A001",
+    nm_brg: "Kopi Bubuk 100g",
+    hrg: "15000",
+    ket_stok: "ada"
+  },
+  {
+    kd_produk: "A002",
+    nm_brg: "Teh Celup 50g",
+    hrg: "12000",
+    ket_stok: "habis"
+  },
+  {
+    kd_produk: "A003",
+    nm_brg: "Keripik Pisang",
+    hrg: "10000",
+    ket_stok: "ada"
+  }
+];
+
+module.exports = vendorAData;
